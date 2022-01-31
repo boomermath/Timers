fb779a5c88add87cec85e7624d3c87b3de83eada
boomermath <boomermath13@gmail.com>
2022-01-30 19:12:58 -0500

    Angular finished

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 ## What is it about?
 
 I made a repository with the same timer recreated in each framework, just to see how each framework differs.
-
-## Angular in progress
--- angular-timer/src/app/app.component.ts
+++ angular-timer/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-timer';
+  seconds = 0;
+  timer?: NodeJS.Timeout;
+
+  get secondsDisplay() {
+    return new Date(this.seconds * 1000).toISOString().substring(11, 19)
+  }
+
+  start() {
+    this.timer = setInterval(() => {
+      this.seconds++;
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.timer!);
+  }
+
+  reset() {
+    this.seconds = 0;
+    this.stop();
+  }
 }
