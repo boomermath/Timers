d911b6e6ac932bcd5b36be9bf6d75b95a79ab433
boomermath <boomermath13@gmail.com>
2022-01-30 19:14:00 -0500

    Add cleanup function

--- angular-timer/src/app/app.component.ts
+++ angular-timer/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   seconds = 0;
   timer?: NodeJS.Timeout;
 
+  ngOnDestroy() {
+    this.stop();
+  }
+
   get secondsDisplay() {
-    return new Date(this.seconds * 1000).toISOString().substring(11, 19)
+    return new Date(this.seconds * 1000).toISOString().substring(11, 19);
   }
 
   start() {
